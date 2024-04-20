562f7776c8caa8fc479a2010bbb9810dc2d2a8f0
lv1codeman <lv1codeman@gmail.com>
2024-04-21 02:55:19 +0800

    子傳父、模板引用

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import SecondLayer from "./components/SecondLayer.vue";
 
-const msg = ref("msg2 to child component.");
-const array1 = [
-  { id: 1, name: "Kiangkiang" },
-  { id: 2, name: "Xiaohong" },
-  { id: 3, name: "Xiaoma" },
-];
-const object1 = {
-  name: "Xiaoma",
-  age: "18",
-  gender: "Boy",
-};
 //接收子組件傳來的資料
 const dataFromSon = ref("");
+const dataFromSon2 = ref("");
 const fromSon = (data) => {
   dataFromSon.value = data;
   console.log(dataFromSon.value);
 };
+const fromSon2 = (data) => {
+  dataFromSon2.value = data;
+  console.log(dataFromSon2.value);
+};
+
+const childRef = ref(null);
+const showChildName = ref(null);
+
+// <SecondLayer ref="childRef"></SecondLayer>
+// ref="childRef"為模板引用，需在onMounted()掛載完畢以後才會生效，如果沒有寫在onMounted()中，那就是還在setup的時候，也就是beforeCreate和created之後，此時還沒有mount所以模板引用無法生效
+onMounted(() => {
+  console.log(childRef.value);
+  //調用子組件的setName()修改name
+  childRef.value.setName();
+  //因為被setName()修改name所以結果為"New name"
+  showChildName.value = childRef.value.name;
+});
 </script>
 <template>
   <h3>This is App.vue</h3>
-  <div>子組件傳過來的資料: {{ dataFromSon }}</div>
+  <div>子組件傳過來的資料1: {{ dataFromSon }}</div>
+  <div>子組件傳過來的資料2: {{ showChildName }}</div>
+  <div>子組件的name: {{ dataFromSon2 }}</div>
   <hr />
-  <h3>傳給子組件的資料</h3>
-  <input v-model="msg" placeholder="key something" />
-  <div>msg: {{ msg }}</div>
   <div class="pend">
-    <SecondLayer
-      msg1="msg1 to child component."
-      :msg2="msg"
-      :arrMsg="array1"
-      :objMsg="object1"
-      @toPAPA="fromSon"
-    >
+    <SecondLayer @toPAPA="fromSon" @toPAPA2="fromSon2" ref="childRef">
     </SecondLayer>
   </div>
 </template>
--- src/components/SecondLayer.vue
+++ src/components/SecondLayer.vue
@@ -1,31 +1,23 @@
 <script setup>
-defineProps({
-  msg1: String,
-  msg2: String,
-  arrMsg: Array,
-  objMsg: Object,
-});
-// 或是
-// const props = defineProps(["msg1", "msg2", "arrMsg", "objMsg"]);
-
-const emit = defineEmits(["toPAPA"]);
+import { ref } from "vue";
+const emit = defineEmits(["toPAPA", "toPAPA2"]);
 emit("toPAPA", "msg from SecondLayer.vue");
+emit("toPAPA2", "msg2 from SecondLayer.vue");
+
+const name = ref("test name");
+const setName = () => {
+  name.value = "New name";
+};
+
+defineExpose({
+  name,
+  setName,
+});
 </script>
 <template>
   <h3>This is SecondLayer.vue</h3>
-  <h3>父組件傳過來的資料</h3>
-  <div>msg1 from app.vue: {{ msg1 }}</div>
-  <div>msg2 from app.vue: {{ msg2 }}</div>
-  <div>The name of first element in arrMsg: {{ arrMsg[0].name }}</div>
-  arrMsg:
-  <div v-for="item in arrMsg" :key="item.id">
-    &nbsp;&nbsp;{{ item.id }}-{{ item.name }}
-  </div>
-  <div>object1.name = {{ objMsg.name }}</div>
-  <div>object1.age = {{ objMsg.age }}</div>
-  <div>object1.gender = {{ objMsg.gender }}</div>
+  <div>{{ name }}</div>
 </template>
-
 <style scope>
 .pend {
   padding-left: 20px;
